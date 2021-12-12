8837a72707fd0d75b108c7302886dc09d4931ada
binhnguyen3515 <binhnguyen3515@gmail.com>
2021-12-13 04:16:00 +0700

    fix header image

--- src/app/common/globalVariable.ts
+++ src/app/common/globalVariable.ts
@@ -1,5 +1,6 @@
 export const GlobalVariable = Object.freeze({
     baseUrl : 'https://angular-shopping-ps14048.herokuapp.com/v1/api/',
     baseHostImageUrl : 'https://angular-shopping-ps14048.herokuapp.com/assets/images/',
-    baseHostAvatarUrl : 'https://angular-shopping-ps14048.herokuapp.com/assets/avatar/'
+    baseHostAvatarUrl : 'https://angular-shopping-ps14048.herokuapp.com/assets/avatar/',
+    baseHeaderImageUrl:'https://binhnguyen3515.github.io/angular-shopping-ps14048/assets/banner/'
 })
--- src/app/component_home/header/header.component.ts
+++ src/app/component_home/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {GlobalVariable} from 'src/app/common/globalVariable';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  
+  headerImageUrl = GlobalVariable.baseHeaderImageUrl
   constructor() { }
 
   ngOnInit() {
